ee14741f77d149a8808cf7b3ac30856747cdda19
marenwoodruff <marenwoodruff@gmail.com>
2020-06-23 22:13:24 -0700

    linked reviews to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_040300) do
+ActiveRecord::Schema.define(version: 2020_06_24_045234) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_06_24_040300) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "restaurant_id"
   end
 
   create_table "users", force: :cascade do |t|
