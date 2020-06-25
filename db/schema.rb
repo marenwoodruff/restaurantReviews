16a8c1000657191a15f842676a94cc1379a82f55
marenwoodruff <marenwoodruff@gmail.com>
2020-06-24 22:23:31 -0700

    added first + last name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_045234) do
+ActiveRecord::Schema.define(version: 2020_06_25_044025) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,9 @@ ActiveRecord::Schema.define(version: 2020_06_24_045234) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
